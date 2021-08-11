fb58c81c49ce6d1cb8cf8298bba73d4c42405464
David Bouscarle <d.bouscarle@gmail.com>
2021-08-11 14:30:40 +0200

    Add Async method to the request

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/test', (req, res) => {
+app.get('/test', async (req, res) => {
     res.json('Hello World!');
 });
 
