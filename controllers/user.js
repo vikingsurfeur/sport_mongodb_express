b3af8604eef46a3d577c39160f85393995ccbdd0
David Bouscarle <d.bouscarle@gmail.com>
2021-08-12 16:55:13 +0200

    Add the delete method to the controllers

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const userGet = async (req, res) => {
         const MyUsers = await User.find();
         res.json(MyUsers);
     } catch (error) {
+        res.json(error.message);
         console.error(`Something get Wrong: \n${error.message}`);
     }
 };
@@ -12,10 +13,10 @@ const userCreate = async (req, res) => {
     try {
         const User = req.app.get('models').User;
         const NewUser = await new User({
-            firstName: "Jean",
-            lastName: "Reno",
-            dateOfBirth: new Date(),
-            email: "j.reno@example.com",
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            dateOfBirth: req.body.dateOfBirth,
+            email: req.body.email,
         }).save();
         res.json(NewUser);
     } catch (error) {
@@ -24,4 +25,22 @@ const userCreate = async (req, res) => {
     }
 };
 
-module.exports = { userGet, userCreate };
+const userDelete = async (req, res) => {
+    try {
+        if (!req.body._id) {
+            return res.json({
+                status: false,
+                message: 'No id provided',
+            });
+        }
+        const User = req.app.get('models').User;
+        const MyUser = await User.findById(req.body._id);
+        await MyUser.remove();
+        res.json('Successfully Deleted');
+    } catch (error) {
+        res.json(error.message);
+        console.error(`Something get Wrong: \n${error.message}`);
+    }
+};
+
+module.exports = { userGet, userCreate, userDelete };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
-const { userGet, userCreate } = require('../controllers/user');
+const { userGet, userCreate, userDelete } = require('../controllers/user');
 
 const userRoute = (app) => {
     // Create
     app.post('/create', userCreate);
     // Read
     app.get('/user', userGet);
+    // Delete
+    app.post('/delete', userDelete);
 };
 
 module.exports = userRoute;
