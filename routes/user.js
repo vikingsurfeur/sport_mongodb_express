22c20d465ef0d033e16587f7d462f5f4a5565123
David Bouscarle <d.bouscarle@gmail.com>
2021-08-13 09:03:39 +0200

    Modify the name of routes

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,13 @@ const { userGet, userCreate, userDelete, userUpdate } = require('../controllers/
 
 const userRoute = (app) => {
     // Read
-    app.get('/user', userGet);
+    app.get('/users', userGet);
     // Create
-    app.post('/create', userCreate);
+    app.post('/user-create', userCreate);
     // Delete
-    app.post('/delete', userDelete);
+    app.post('/user-delete', userDelete);
     // Update
-    app.post('/update', userUpdate);
+    app.post('/user-update', userUpdate);
 };
 
 module.exports = userRoute;
